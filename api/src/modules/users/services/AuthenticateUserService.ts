78a085ac1a11983c09dd8aadc3b091e3445a7cef
Eloilson Rocha <eloilson.ageedu@gmail.com>
2020-09-06 16:29:39 -0300

    refactoring authenticate user test

--- api/src/modules/users/services/AuthenticateUserService.ts
+++ api/src/modules/users/services/AuthenticateUserService.ts
@@ -1,12 +1,12 @@
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
-import IUsersRepository from '../repositories/IUsersRepository'
-import IHashProvider from '../providers/HashProvider/models/IHashProvider';
-
 import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppError';
+
 import User from '../infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
 
 interface IRequest {
   email: string;
@@ -29,14 +29,16 @@ class AuthenticateUserService {
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
-
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email and password combination.', 401);
     }
 
-    const passwordMatched = await this.hashProvider.compareHash(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email and password combination.', 401);
